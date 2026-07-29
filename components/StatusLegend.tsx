750cb1ebc84c607cc0caf7615471f6dae0da75bd
darknessdearm <mei.n.s.1998@gmail.com>
2026-07-29 21:20:02 +0700

    update data and color

--- components/StatusLegend.tsx
+++ components/StatusLegend.tsx
@@ -2,7 +2,7 @@
 export default function StatusLegend() {
   return (
     <ul className="mt-4 flex flex-wrap items-center gap-x-6 gap-y-2 text-panel">
-      <li className="text-scene">Status</li>
+      <li className="text-scene">Player Status</li>
       <li className="text-fate-alive">· Alive</li>
       <li className="text-fate-dead">· Dead</li>
       <li className="text-fate-lost">· Lost</li>
